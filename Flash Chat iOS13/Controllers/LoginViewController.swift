5a5d1a89ffcb678f9d9a00cf2fad3c303cc6d1d2
Jorge Gonzalez <jagg.dev.27@gmail.com>
2022-05-13 00:38:03 -0500

    Register new users, login and logout using Firebase. Static keyword and UITableView use.

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -15,6 +16,18 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+              guard let strongSelf = self else { return }
+              
+                if let authError = error {
+                    print(authError.localizedDescription)
+                    
+                } else {
+                    strongSelf.performSegue(withIdentifier: K.loginSegue, sender: strongSelf)
+                }
+            }
+        }
     }
     
 }
